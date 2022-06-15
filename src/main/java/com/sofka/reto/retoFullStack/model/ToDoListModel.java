2fbb86562716e6cbacf195dcedd77ec699e4511e
JEAR2 <jhedacro@gmail.com>
2022-06-15 17:34:14 -0500

    Creación de entidades y relaciones

--- src/main/java/com/sofka/reto/retoFullStack/model/ToDoListModel.java
+++ src/main/java/com/sofka/reto/retoFullStack/model/ToDoListModel.java
@@ -1,12 +1,22 @@
 package com.sofka.reto.retoFullStack.model;
 
+import lombok.Data;
 
 import javax.persistence.*;
 
 
+@Data
 @Entity
 @Table(name = "todolist")
 public class ToDoListModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private boolean isCompleted;
 
+    @ManyToOne
+    @JoinColumn(name = "id_todo")
+    private ToDoModel todo;
 
 }
--- src/main/java/com/sofka/reto/retoFullStack/model/ToDoModel.java
+++ src/main/java/com/sofka/reto/retoFullStack/model/ToDoModel.java
@@ -3,11 +3,17 @@ package com.sofka.reto.retoFullStack.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.List;
 @Data
 @Entity
 @Table(name = "todo")
 public class ToDoModel {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "todo")
+    private List<ToDoListModel> toDoList;
 
 
 
